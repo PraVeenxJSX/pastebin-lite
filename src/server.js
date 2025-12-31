fe352e22f81d3a6f138b594781a597379d213963
PraVeenxJSX <160173872+PraVeenxJSX@users.noreply.github.com>
2025-12-31 10:23:44 +0530

    render deployment

--- src/server.js
+++ src/server.js
@@ -4,9 +4,16 @@ import app from "./app.js";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch(console.error);
-
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("DB connection error:", err);
+  });
